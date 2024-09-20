a12caa2acf8a9193bfe3ccb443dc545828336a4f
Cadence Ember <cadence@disroot.org>
2024-09-21 00:33:23 +1200

    Relicense to BSD 3-Clause

--- info.rkt
+++ info.rkt
@@ -4,4 +4,4 @@
 (define deps '("base" "typed-racket-lib" "rackunit-lib"))
 (define build-deps '("racket-doc" "scribble-lib" "typed-racket-doc" "sandbox-lib"))
 (define scribblings '(("scribblings/microyaml.scrbl")))
-(define license 'GPL-3.0-or-later)
+(define license 'BSD-3-Clause)
