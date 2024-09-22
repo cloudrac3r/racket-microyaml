fa8fecdfd2ee3b971409e424bca128211bac79f4
Cadence Ember <cadence@disroot.org>
2024-09-23 11:11:39 +1200

    v1.1

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "microyaml")
-(define version "1.0")
+(define version "1.1")
 (define deps '("base" "typed-racket-lib" "rackunit-lib"))
 (define build-deps '("racket-doc" "scribble-lib" "typed-racket-doc" "sandbox-lib"))
 (define scribblings '(("scribblings/microyaml.scrbl")))
